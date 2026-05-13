6c95ba4d6eb1a87c38a6dd23f5d36500382381d0
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2026-05-13 14:44:37 +0200

    fix linting issues

--- packages/database/src/json-types.ts
+++ packages/database/src/json-types.ts
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-relative-packages -- required for importing types */
 /* eslint-disable @typescript-eslint/no-namespace -- using namespaces is required for prisma-json-types-generator */
 import { type TActionClassNoCodeConfig } from "@formbricks/types/action-classes";
 import type { TOrganizationAccess } from "@formbricks/types/api-key";
